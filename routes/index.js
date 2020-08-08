bb05a4efb86f4fc88be6e8ab0f6221fb315ece45
Ezekiel <iamwebwiz@gmail.com>
2020-08-08 23:36:18 +0100

    Update index routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { index, users } from "../controllers/SampleController";
+import { index } from "../controllers/SampleController";
+
 const router = express.Router();
 
 router.get("/", index);
-router.get("/users", users);
 
 export default router;
