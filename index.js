dbee6001cc9a9bc31a330305502a1598318058fa
Ezekiel <iamwebwiz@gmail.com>
2020-08-08 23:36:36 +0100

    Add users route to entry file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import router from "./routes";
+import indexRoutes from "./routes";
+import userRoutes from "./routes/users";
 import connectDB from "./config/database";
 require("dotenv").config();
 
@@ -8,6 +9,7 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/", router);
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
