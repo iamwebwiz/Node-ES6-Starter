838dc6b630e5e4fe4127fdfc4e3e5a969f629b2d
Ezekiel <iamwebwiz@gmail.com>
2020-08-08 23:36:49 +0100

    Refactor SampleController

--- controllers/SampleController.js
+++ controllers/SampleController.js
@@ -1,14 +1,3 @@
-import User from "../models/User";
-
 export const index = (req, res) => {
   res.json({ message: "Hello Express!" });
-};
-
-export const users = async (req, res) => {
-  const users = await User.find({});
-
-  res.json({
-    message: "Users fetched.",
-    users,
-  });
 };
