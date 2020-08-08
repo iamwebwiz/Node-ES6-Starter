55ce92b9f95ac261751979c53dc1afc285120351
Ezekiel <iamwebwiz@gmail.com>
2020-08-08 23:30:16 +0100

    Update index file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import router from "./routes";
+import connectDB from "./config/database";
+require("dotenv").config();
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/", router);
 
-app.listen(port);
+app.listen(port, () => console.log(`Server started at port ${port}`));
