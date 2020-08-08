4f829e8964083bad8db020f7ea6349ac03de021d
Ezekiel <iamwebwiz@gmail.com>
2020-08-08 23:30:07 +0100

    Update SampleController

--- controllers/SampleController.js
+++ controllers/SampleController.js
@@ -1,3 +1,14 @@
+import User from "../models/User";
+
 export const index = (req, res) => {
   res.json({ message: "Hello Express!" });
+};
+
+export const users = async (req, res) => {
+  const users = await User.find({});
+
+  res.json({
+    message: "Users fetched.",
+    users,
+  });
 };
